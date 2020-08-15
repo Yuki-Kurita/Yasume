ae25ca035ccc83fa3689cd31901ecab2644e3156
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-08-16 01:00:28 +0900

    routerの追加:tada:

--- server/index.js
+++ server/index.js
@@ -4,8 +4,20 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("we have a new connection!!!");
+
+  socket.on("disconnect", () => {
+    console.log("USER had left !!!");
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
