d56b74f36a706cb4ba783cebaf7128c2d9a5beb7
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-08-16 01:34:16 +0900

    error hadlingの追加

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = () => {
+import "./Chat.css";
+
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room }, () => {});
+  }, [ENDPOINT, location.search]);
   return <h1>Chat</h1>;
 };
 
