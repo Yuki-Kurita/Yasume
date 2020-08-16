925aaec0057ee182ec5b061d59f9aa370587e831
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-08-16 12:08:11 +0900

    header, inputコンポーネントの分割・追加

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
-
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
 import "./Chat.css";
 
 let socket;
@@ -50,12 +51,11 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
       <div className="container">
-        <input
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={(event) =>
-            event.key === "Enter" ? sendMessage(event) : null
-          }
+        <InfoBar room={room} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
         />
       </div>
     </div>
