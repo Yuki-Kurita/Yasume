ab254d7713ece6dfe0a2ad8ab248827ab84ef90d
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-08-16 13:00:01 +0900

    messageコンポーネントの追加:art:

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,6 +3,7 @@ import queryString from "query-string";
 import io from "socket.io-client";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
 import "./Chat.css";
 
 let socket;
@@ -52,6 +53,7 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
         <Input
           message={message}
           setMessage={setMessage}
