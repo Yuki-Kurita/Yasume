dc7acb16d89a37547a76057163642ee5c0acec37
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-08-16 11:41:58 +0900

    メッセージ機能の追加

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,6 +9,9 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
   const ENDPOINT = "localhost:5000";
 
   useEffect(() => {
@@ -20,8 +23,43 @@ const Chat = ({ location }) => {
     setRoom(room);
 
     socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
   }, [ENDPOINT, location.search]);
-  return <h1>Chat</h1>;
+
+  // mount時とmessagesに変化があった時のみ実行
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (message) {
+      console.log(`send: ${message}`);
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) =>
+            event.key === "Enter" ? sendMessage(event) : null
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
