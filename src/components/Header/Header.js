c68f974dbbb69ce962675d033f22ad18d9fb211d
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-08-21 01:37:11 +0900

    JOINページのデザイン変更:art:

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+  <div className="header">
+    <div className="appLogo">Yasume</div>
+  </div>
+);
+
+export default Header;
